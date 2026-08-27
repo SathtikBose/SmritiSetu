fb82140002bbd5a27916040d7ce3b2e8fe3de82c
SathtikBose <sathtikbose@gmail.com>
2026-08-27 23:07:47 +0530

    feat: initialize project with Expo Router, NativeWind, and TypeScript configuration

--- SmritiSetu/src/app/index.tsx
+++ SmritiSetu/src/app/index.tsx
@@ -1,17 +1,15 @@
 import { Text, View, StyleSheet } from "react-native";
 
+
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 items-center justify-center bg-slate-900 p-4">
+      <Text className="text-2xl font-bold text-indigo-400 mb-2">
+        SmritiSetu
+      </Text>
+      <Text className="text-base text-slate-300 text-center">
+        Edit src/app/index.tsx to edit this screen.
+      </Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
